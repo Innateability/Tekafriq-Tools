0cf6df6501c3209cb879fd392a43b04692bf2e16
Uzuegbunam Godswill <uzuegbunamgodswill2006@gmail.com>
2026-05-22 14:21:58 +0100

    add d

--- Opportunity Tracker/app/__init__.py
+++ Opportunity Tracker/app/__init__.py
@@ -21,6 +21,8 @@ def create_app():
     app = Flask(__name__)
 
     app.config.from_object("config.Config")
+    
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 
     app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
     
